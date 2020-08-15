51875b35dcf68128ba19d333722a2bd8dbbfedba
sigvant <60826785+sigvant@users.noreply.github.com>
2020-08-15 14:47:43 -0300

    Web app main files.
    All the files required to run the application.

---
+++ yelp-final/seeds.js
@@ -0,0 +1,48 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment   = require("./models/comment");
+
+var seeds = [
+    {
+        name: "Cloud's Rest", 
+        image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Desert Mesa", 
+        image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Canyon Floor", 
+        image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    }
+];
+
+async function seedDB(){
+	try {
+		await Campground.remove({});
+		console.log("Campgrounds Removed")
+		await Comment.remove({});
+		console.log("Comments Removed")	
+		for(const seed of seeds) { //this is like a for each but looks like python
+			let campground = await Campground.create(seed);
+			console.log("Campgrounds Created")
+			let comment = await Comment.create(
+				{
+					text:"This place is great, but I wish there was internet",
+					author:"Homer"
+				}
+			)
+			console.log('Comment Created');
+			campground.comments.push(comment);
+			campground.save();
+			console.log("Comment Added to Campground");
+		}
+	} catch(err) {
+		console.log(err);		
+	}
+}
+
+module.exports = seedDB;
